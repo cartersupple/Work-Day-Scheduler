a7ecd97a0c91694f15654df25ee41d8c41c490da
Carter Supple <Cartersupple123@gmail.com>
2021-10-21 02:43:00 -0700

    added variable to store each hours task

--- script.js
+++ script.js
@@ -30,7 +30,72 @@ function init() {
     saveTasks();
     displayTasks();
 }
-
+// adds header date
 getHeaderDate();
 
-
+// created variables to store task data
+var myDay = [
+    {
+        id:"0",
+        hour: "09",
+        time:"09",
+        task:"",
+        amOrPm:"am"
+    },
+    {
+        id:"1",
+        hour: "10",
+        time:"10",
+        task:"",
+        amOrPm:"am"
+    },
+    {
+        id:"2",
+        hour: "11",
+        time:"11",
+        task:"",
+        amOrPm:"am"
+    },
+    {
+        id:"3",
+        hour: "12",
+        time:"12",
+        task:"",
+        amOrPm:"pm"
+    },
+    {
+        id:"4",
+        hour: "01",
+        time:"13",
+        task:"",
+        amOrPm:"pm"
+    },
+    {
+        id:"5",
+        hour: "02",
+        time:"14",
+        task:"",
+        amOrPm:"pm"
+    },
+    {
+        id:"6",
+        hour: "03",
+        time:"14",
+        task:"",
+        amOrPm:"pm"
+    },
+    {
+        id:"7",
+        hour: "04",
+        time:"15",
+        task:"",
+        amOrPm:"pm"
+    },
+    {
+        id:"8",
+        hour: "05",
+        time:"16",
+        task:"",
+        amOrPm:"pm"
+    },
+]
